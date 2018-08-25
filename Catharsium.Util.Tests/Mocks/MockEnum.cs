d67415b94ed9f7b95b5a13b9474c0c3966360e66
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2018-08-25 14:56:06 +0200

    Cleanup namespaces

--- Catharsium.Util.Tests/Mocks/MockEnum.cs
+++ Catharsium.Util.Tests/Mocks/MockEnum.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Catharsium.Util.Tests.Mocks
+﻿namespace Catharsium.Util.Tests.Mocks
 {
     public enum MockEnum
     {
