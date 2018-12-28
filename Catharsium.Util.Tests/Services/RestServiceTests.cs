516f92ba5ff18a594a0cf0cf3a3db1271b2f2efb
Thierry Brachthuizer <tbrachthuizer@zetsolutions.nl>
2018-12-28 15:44:57 +0100

    Add generic RestService

---
+++ Catharsium.Util.Tests/Services/RestServiceTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Catharsium.Util.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using RestSharp;
+
+namespace Catharsium.Util.Tests.Services
+{
+    [TestClass]
+    public class RestServiceTests
+    {
+        #region Fixture
+
+        private IRestClient RestClient { get; set; }
+
+        private RestService Target { get; set; }
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.RestClient = Substitute.For<IRestClient>();
+            this.Target = new RestService(this.RestClient);
+        }
+
+        #endregion
+
+        #region PostToJsonService
+
+        [TestMethod]
+        public void PostToJsonService_ValidData_CallsServiceWith()
+        {
+            var response = new RestResponse {
+                StatusCode = HttpStatusCode.OK,
+                ResponseStatus = ResponseStatus.Completed,
+                Content = "1"
+            };
+            this.RestClient.Execute(Arg.Any<RestRequest>()).Returns(response);
+            var resource = "My resource";
+            var data = "My data";
+
+            this.Target.PostToJsonService(resource, data);
+            this.RestClient.Received().Execute(Arg.Is<RestRequest>(r => IsExpectedRequest(r, resource, "application/json", data)));
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(HttpRequestException))]
+        public void PostToJsonService_InvalidResponse_ThrowsException()
+        {
+            var response = new RestResponse {
+                StatusCode = HttpStatusCode.InternalServerError,
+                ResponseStatus = ResponseStatus.Error
+            };
+            this.RestClient.Execute(Arg.Any<RestRequest>()).Returns(response);
+            var resource = "My resource";
+            var data = "My data";
+
+            this.Target.PostToJsonService(resource, data);
+            this.RestClient.Received().Execute(Arg.Is<RestRequest>(r => IsExpectedRequest(r, resource, "application/json", data)));
+        }
+
+        #endregion
+
+        #region PostToJsonServiceWithLongResponseType
+
+        [TestMethod]
+        public void PostToJsonServiceWithLongResponseType_ValidData_CallsServiceWith()
+        {
+            var responseContent = long.MaxValue;
+            var response = new RestResponse {
+                StatusCode = HttpStatusCode.OK,
+                ResponseStatus = ResponseStatus.Completed,
+                Content = responseContent.ToString()
+            };
+            this.RestClient.Execute(Arg.Any<RestRequest>()).Returns(response);
+            var resource = "My resource";
+            var data = "My data";
+
+            var actual = this.Target.PostToJsonServiceWithLongResponseType(resource, data);
+            Assert.AreEqual(responseContent, actual);
+        }
+
+        #endregion
+
+        #region Support methods
+
+        private static bool IsExpectedRequest(IRestRequest request, string resource, string bodyName, string bodyContents)
+        {
+            var body = request.Parameters.FirstOrDefault(p => p.Type == ParameterType.RequestBody);
+            if (body == null) {
+                return bodyContents == null;
+            }
+
+            return request.Resource == resource &&
+                   request.Method == Method.POST &&
+                   body.Name == bodyName &&
+                   body.Value.ToString().Contains(bodyContents);
+        }
+
+        #endregion
+    }
+}
---
+++ Catharsium.Util/Services/RestService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using RestSharp;
+
+namespace Catharsium.Util.Services
+{
+    public class RestService
+    {
+        private readonly IRestClient client;
+
+
+        public RestService(IRestClient client)
+        {
+            this.client = client;
+        }
+
+
+        public string PostToJsonService(string resource, object data)
+        {
+            var request = new RestRequest(resource, Method.POST);
+            request.AddHeader("Content-type", "application/json");
+            request.AddJsonBody(data);
+            var response = this.client.Execute(request);
+            if (!response.IsSuccessful) {
+                throw new HttpRequestException($"Unsuccessfull call to {resource}");
+            }
+
+            return response.Content;
+        }
+
+
+        public long PostToJsonServiceWithLongResponseType(string resource, object data)
+        {
+            return long.Parse(this.PostToJsonService(resource, data));
+        }
+    }
+}
