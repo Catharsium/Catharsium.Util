9029f10fc898b94c29ba3703bba7b185d2a1b04d
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2019-02-13 10:11:18 +0100

    #6 Rename ServiceCollectionExtensions.WasRegistered > ServiceCollectionExtensions.ReceivedRegistration

--- Catharsium.Util.Testing.Tests/Extensions/ServiceCollectionExtensionsTests.cs
+++ Catharsium.Util.Testing.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -10,46 +10,46 @@ namespace Catharsium.Util.Testing.Tests.Extensions
     [TestClass]
     public class ServiceCollectionExtensionsTests
     {
-        #region WasRegistered<I>
+        #region ReceivedRegistration<I>
 
         [TestMethod]
-        public void WasRegistered_ServiceCollectionAddCalled_DoesNotThrowAnException()
+        public void ReceivedRegistration_ServiceCollectionAddCalled_DoesNotThrowAnException()
         {
             var target = Substitute.For<IServiceCollection>();
             target.AddScoped<IMockInterface1>();
-            ServiceCollectionExtensions.WasRegistered<IMockInterface1>(target);
+            ServiceCollectionExtensions.ReceivedRegistration<IMockInterface1>(target);
         }
 
 
         [TestMethod]
         [ExpectedException(typeof(ReceivedCallsException))]
-        public void WasRegistered_DifferentInterfaceRegistered_ThrowAnException()
+        public void ReceivedRegistration_DifferentInterfaceRegistered_ThrowAnException()
         {
             var target = Substitute.For<IServiceCollection>();
             target.AddScoped<IMockInterface1>();
-            ServiceCollectionExtensions.WasRegistered<IMockInterface2>(target);
+            ServiceCollectionExtensions.ReceivedRegistration<IMockInterface2>(target);
         }
 
         #endregion
 
-        #region WasRegistered<I, T>
+        #region ReceivedRegistration<I, T>
 
         [TestMethod]
-        public void WasRegistered_WithImplementation_ServiceCollectionAddCalled_DoesNotThrowAnException()
+        public void ReceivedRegistration_WithImplementation_ServiceCollectionAddCalled_DoesNotThrowAnException()
         {
             var target = Substitute.For<IServiceCollection>();
             target.AddScoped<IMockInterface1, Mock1>();
-            ServiceCollectionExtensions.WasRegistered<IMockInterface1, Mock1>(target);
+            ServiceCollectionExtensions.ReceivedRegistration<IMockInterface1, Mock1>(target);
         }
 
 
         [TestMethod]
         [ExpectedException(typeof(ReceivedCallsException))]
-        public void WasRegistered_WithImplementation_DifferentImplementationRegistered_ThrowAnException()
+        public void ReceivedRegistration_WithImplementation_DifferentImplementationRegistered_ThrowAnException()
         {
             var target = Substitute.For<IServiceCollection>();
             target.AddScoped<IMockInterface1, Mock2>();
-            ServiceCollectionExtensions.WasRegistered<IMockInterface1, Mock1>(target);
+            ServiceCollectionExtensions.ReceivedRegistration<IMockInterface1, Mock1>(target);
         }
 
         #endregion
--- Catharsium.Util.Testing/Extensions/ServiceCollectionExtensions.cs
+++ Catharsium.Util.Testing/Extensions/ServiceCollectionExtensions.cs
@@ -5,13 +5,13 @@ namespace Catharsium.Util.Testing.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static void WasRegistered<I>(this IServiceCollection serviceCollection)
+        public static void ReceivedRegistration<I>(this IServiceCollection serviceCollection)
         {
             serviceCollection.Received().Add(Arg.Is<ServiceDescriptor>(d => d.ServiceType == typeof(I)));
         }
 
 
-        public static void WasRegistered<I, T>(this IServiceCollection serviceCollection)
+        public static void ReceivedRegistration<I, T>(this IServiceCollection serviceCollection)
         {
             serviceCollection.Received().Add(Arg.Is<ServiceDescriptor>(d => d.ServiceType == typeof(I) && d.ImplementationType == typeof(T)));
         }
