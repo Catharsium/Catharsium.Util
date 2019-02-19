8548ffc1e295c670b5d587173459b861a4b770f7
Thierry Brachthuizer <tbrachthuizer@zetsolutions.nl>
2019-02-19 08:31:14 +0100

    Add GroupRequiredAttribute

--- Catharsium.Util.Tests/_Mocks/MockObject.cs
+++ Catharsium.Util.Tests/_Mocks/MockObject.cs
@@ -1,9 +1,19 @@
-﻿namespace Catharsium.Util.Tests._Mocks
+﻿using Catharsium.Util.Validation;
+
+namespace Catharsium.Util.Tests._Mocks
 {
     internal class MockObject
     {
+        [GroupRequired("Group 1")]
         public string Property1 { get; set; }
+
+        [GroupRequired("Group 1")]
         public string Property2 { get; set; }
+
+        [GroupRequired("Group 2")]
         public string Property3 { get; set; }
+
+        [GroupRequired("Group 2")]
+        public string Property4 { get; set; }
     }
 }
---
+++ Catharsium.Util/Validation/GroupRequiredAttribute.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Catharsium.Util.Validation
+{
+    public class GroupRequiredAttribute : ValidationAttribute
+    {
+        private string GroupName { get; }
+
+
+        public GroupRequiredAttribute(string groupName)
+        {
+            this.GroupName = groupName;
+        }
+
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var isValid = this.IsValid(validationContext.ObjectInstance, validationContext.MemberName);
+            if (isValid) {
+                return ValidationResult.Success;
+            }
+
+            var message = new StringBuilder();
+            message.Append(validationContext.MemberName);
+            return new ValidationResult($"One of {message} is required");
+        }
+
+
+        public bool IsValid(object model, string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) {
+                return true;
+            }
+
+            var groupMembersAreEmpty = new List<bool>();
+            var type = model.GetType();
+            foreach (var property in type.GetProperties()) {
+                var attributes = property.GetCustomAttributes(typeof(GroupRequiredAttribute), false)
+                    .OfType<GroupRequiredAttribute>()
+                    .Where(a => a.GroupName == this.GroupName);
+                if (attributes.Any()) {
+                    var propertyValue = property.GetValue(model);
+                    groupMembersAreEmpty.Add(propertyValue == null || string.IsNullOrWhiteSpace(propertyValue.ToString()));
+                }
+            }
+
+            return groupMembersAreEmpty.Any(m => !m);
+        }
+    }
+}
