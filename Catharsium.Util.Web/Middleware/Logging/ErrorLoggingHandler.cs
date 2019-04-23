406e89fd8350f6e0941f4fdb973660ac5fc043c5
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2019-04-23 08:38:10 +0200

    #28 Add ErrorLoggingHandler

---
+++ Catharsium.Util.Web/Middleware/Logging/ErrorLoggingHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Catharsium.Util.Web.Middleware.Logging
+{
+    public class ErrorLoggingHandler
+    {
+        private readonly RequestDelegate next;
+
+
+
+        public ErrorLoggingHandler(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+
+        public async Task Invoke(HttpContext httpContext, ILogger<ErrorLoggingHandler> logger)
+        {
+            try
+            {
+                await this.next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Uncaught exception");
+                throw;
+            }
+        }
+    }
+}
