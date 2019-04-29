fabef4f1d25917f9f68b2b4e459427c85ac9dac1
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2019-04-29 15:48:19 +0200

    #14 Add QuickSortExtensions

---
+++ Catharsium.Util.Tests/Sorting/QuickSortExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using Catharsium.Util.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catharsium.Util.Tests.Sorting
+{
+    [TestClass]
+    public class QuickSortExtensionsTests
+    {
+        [TestMethod]
+        public void QuickSort_EmptyList_ReturnsEmptyList()
+        {
+            var input = new decimal[0];
+            var actual = input.QuickSort();
+            Assert.AreEqual(0, actual.Count());
+        }
+
+
+        [TestMethod]
+        public void QuickSort_SingleItem_ReturnsItem()
+        {
+            var input = new[] {0m};
+            var actual = input.QuickSort();
+            Assert.AreEqual(input.Length, actual.Count());
+        }
+
+
+        [TestMethod]
+        public void QuickSort_SortedList_ReturnsUnchanged()
+        {
+            var input = new[] { 1m, 2m, 3m, 4m, 5m, 6m, 7m, 8m, 9m };
+            var actual = input.QuickSort().ToList();
+            Assert.AreEqual(input.Length, actual.Count);
+            for (var i = 0; i < input.Length - 1; i++) {
+                Assert.IsTrue(input[i] == actual[i]);
+            }
+        }
+
+
+        [TestMethod]
+        public void QuickSort_MultipleItems_ReturnsSortedItems()
+        {
+            var input = new[] {1m, 5m, 2m, 7m, 3m, 9m, 8m, 4m, 6m};
+            var actual = input.QuickSort().ToList();
+            Assert.AreEqual(input.Length, actual.Count);
+            for (var i = 1; i < input.Length - 1; i++) {
+                Assert.IsTrue(actual[i - 1] < actual[i]);
+                Assert.IsTrue(actual[i] < actual[i + 1]);
+            }
+        }
+    }
+}
---
+++ Catharsium.Util/Sorting/QuickSortExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Catharsium.Util.Sorting
+{
+    public static class QuickSortExtensions
+    {
+        public static IEnumerable<decimal> QuickSort(this IEnumerable<decimal> items) //where T : IComparable<T>
+        {
+            var itemsList = items.ToList();
+            var result = new List<decimal>();
+
+            if (!itemsList.Any()) {
+                return result;
+            }
+
+            var pivot = itemsList.FirstOrDefault();
+            itemsList.RemoveAt(0);
+            var smaller = itemsList.Where(i => i < pivot);
+            var equal = itemsList.Where(i => i == pivot);
+            var greater = itemsList.Where(i => i > pivot);
+            result.AddRange(smaller.QuickSort());
+            result.Add(pivot);
+            result.AddRange(equal);
+            result.AddRange(greater.QuickSort());
+
+            return result;
+        }
+    }
+}
