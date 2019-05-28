aa945af26a3b793c0030834934c922afb4f8a587
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2019-05-28 07:54:31 +0200

    Housekeeping

--- Catharsium.Util.Testing.Tests/TestFixtureTests/TestFixtureUnitTests.cs
+++ Catharsium.Util.Testing.Tests/TestFixtureTests/TestFixtureUnitTests.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using Catharsium.Util.Testing.Interfaces;
 using Catharsium.Util.Testing.Tests._Mocks;
