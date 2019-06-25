ebe325c6931554492451fbad859687ee8c1c8bbd
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2019-06-25 11:02:44 +0200

    #2 Add ExpectedExceptionMessageAttribute

---
+++ Catharsium.Util.Testing/Attributes/ExpectedExceptionMessageAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catharsium.Util.Testing.Attributes
+{
+    public class ExpectedExceptionMessageAttribute : ExpectedExceptionBaseAttribute
+    {
+        private readonly Type exceptionType;
+        private readonly string messageSubstring;
+
+
+        public ExpectedExceptionMessageAttribute(Type exceptionType, string messageSubstring)
+        {
+            this.exceptionType = exceptionType;
+            this.messageSubstring = messageSubstring;
+        }
+
+
+        protected override void Verify(Exception exception)
+        {
+            Assert.AreEqual(this.exceptionType, exception.GetType());
+            Assert.IsTrue(exception.Message.Contains(this.messageSubstring));
+        }
+
+
+        public void Validate(Exception exception)
+        {
+            this.Verify(exception);
+        }
+    }
+}
