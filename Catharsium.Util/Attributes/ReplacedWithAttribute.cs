fd6bba6a1f89d61b422e1ad8be509c3ce70dca75
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2019-07-02 08:09:09 +0200

    Add ReplacedWithAttribute

---
+++ Catharsium.Util/Attributes/ReplacedWithAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Catharsium.Util.Attributes
+{
+    public class ReplacedWithAttribute : Attribute
+    {
+        private readonly Type replacementType;
+
+
+        public ReplacedWithAttribute(Type replacementType)
+        {
+            this.replacementType = replacementType;
+        }
+
+
+        public override string ToString()
+        {
+            return " " + this.replacementType.FullName;
+        }
+    }
+}
