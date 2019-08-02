d0ecce9f993eb12a5e96e100a3988c634f27aabe
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2019-08-02 08:30:48 +0200

    Release 1.2.12, Expand IConsoleWrapper

--- Catharsium.Util.IO/Interfaces/IConsoleWrapper.cs
+++ Catharsium.Util.IO/Interfaces/IConsoleWrapper.cs
@@ -1,18 +1,31 @@
 ﻿using System;
+using System.IO;
 
 namespace Catharsium.Util.IO.Interfaces
 {
     public interface IConsoleWrapper
     {
-        ConsoleColor ForegroundColor { get; set; }
         ConsoleColor BackgroundColor { get; set; }
-        void ResetColor();
-
+        int BufferHeight { get; set; }
+        int BufferWidth { get; set; }
+        bool CapsLock { get; }
+        int CursorLeft { get; set; }
+        int CursorSize { get; set; }
+        int CursorTop { get; set; }
+        bool CursorVisible { get; set; }
+        ConsoleColor ForegroundColor { get; set; }
+        TextReader In { get; }
+        TextWriter Out { get; }
+        string Title { get; set; }
+        void MoveBufferArea(int sourceLeft, int sourceTop, int sourceWidth, int sourceHeight, int targetLeft, int targetTop);
+        void MoveBufferArea(int sourceLeft, int sourceTop, int sourceWidth, int sourceHeight, int targetLeft, int targetTop, char sourceChar, ConsoleColor sourceForeColor, ConsoleColor sourceBackColor);
         int Read();
         ConsoleKeyInfo ReadKey();
         string ReadLine();
-
+        void ResetColor();
+        void SetBufferSize(int width, int height);
+        void SetCursorPosition(int left, int top);
         void Write(string text);
-        void WriteLine(string text);
+        void WriteLine(string text = null);
     }
 }
--- Catharsium.Util.IO/Wrappers/SystemConsoleWrapper.cs
+++ Catharsium.Util.IO/Wrappers/SystemConsoleWrapper.cs
@@ -1,10 +1,148 @@
 ﻿using Catharsium.Util.IO.Interfaces;
 using System;
+using System.IO;
 
 namespace Catharsium.Util.IO.Wrappers
 {
     public class SystemConsoleWrapper : IConsoleWrapper
     {
+        #region Basic
+
+        public string Title
+        {
+            get {
+                return System.Console.Title;
+            }
+            set {
+                System.Console.Title = value;
+            }
+        }
+
+
+        public bool CapsLock
+        {
+            get {
+                return System.Console.CapsLock;
+            }
+        }
+
+        #endregion
+
+        #region IO
+
+        public TextReader In
+        {
+            get {
+                return System.Console.In;
+            }
+        }
+
+
+        public TextWriter Out
+        {
+            get {
+                return System.Console.Out;
+            }
+        }
+
+        #endregion
+
+        #region Buffer
+
+        public int BufferWidth
+        {
+            get {
+                return System.Console.BufferWidth;
+            }
+            set {
+                System.Console.BufferWidth = value;
+            }
+        }
+
+
+        public int BufferHeight
+        {
+            get {
+                return System.Console.BufferHeight;
+            }
+            set {
+                System.Console.BufferHeight = value;
+            }
+        }
+
+
+        public void SetBufferSize(int width, int height)
+        {
+            System.Console.SetBufferSize(width, height);
+        }
+
+
+        public void MoveBufferArea(int sourceLeft, int sourceTop, int sourceWidth, int sourceHeight, int targetLeft, int targetTop)
+        {
+            System.Console.MoveBufferArea(sourceLeft, sourceTop, sourceWidth, sourceHeight, targetLeft, targetTop);
+        }
+
+
+        public void MoveBufferArea(int sourceLeft, int sourceTop, int sourceWidth, int sourceHeight, int targetLeft, int targetTop, char sourceChar, ConsoleColor sourceForeColor, ConsoleColor sourceBackColor)
+        {
+            System.Console.MoveBufferArea(sourceLeft, sourceTop, sourceWidth, sourceHeight, targetLeft, targetTop, sourceChar, sourceForeColor, sourceBackColor);
+        }
+
+        #endregion
+
+        #region Cursor
+
+        public int CursorLeft
+        {
+            get {
+                return System.Console.CursorLeft;
+            }
+            set {
+                System.Console.CursorLeft = value;
+            }
+        }
+
+
+        public int CursorTop
+        {
+            get {
+                return System.Console.CursorTop;
+            }
+            set {
+                System.Console.CursorTop = value;
+            }
+        }
+
+
+        public int CursorSize
+        {
+            get {
+                return System.Console.CursorSize;
+            }
+            set {
+                System.Console.CursorSize = value;
+            }
+        }
+
+
+        public bool CursorVisible
+        {
+            get {
+                return System.Console.CursorVisible;
+            }
+            set {
+                System.Console.CursorVisible = value;
+            }
+        }
+
+
+        public void SetCursorPosition(int left, int top)
+        {
+            System.Console.SetCursorPosition(left, top);
+        }
+
+        #endregion
+
         #region Color
 
         public ConsoleColor ForegroundColor
