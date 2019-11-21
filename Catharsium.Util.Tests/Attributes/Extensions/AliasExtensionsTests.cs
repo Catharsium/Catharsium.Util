793ae363443ab7597e7ca5440adebdedd3f3cc6e
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2019-11-21 12:51:50 +0100

    Add AliasExtensions.GetAlias

---
+++ Catharsium.Util.Tests/Attributes/Extensions/AliasExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Catharsium.Util.Attributes.Extensions;
+using Catharsium.Util.Tests._Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catharsium.Util.Tests.Attributes.Extensions
+{
+    [TestClass]
+    public class AliasExtensionsTests
+    {
+        [TestMethod]
+        public void GetAlias_ValidIndex_ReturnsAliasAtIndex()
+        {
+            var actual = MockEnum.First.GetAlias(0);
+            Assert.AreEqual("1", actual);
+        }
+
+
+        [TestMethod]
+        public void GetAlias_InvalidIndex_ReturnsFallback()
+        {
+            var fallback = "My fallback";
+            var actual = MockEnum.First.GetAlias(1, fallback);
+            Assert.AreEqual(fallback, actual);
+        }
+    }
+}
---
+++ Catharsium.Util/Attributes/Extensions/AliasExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Catharsium.Util.Attributes.Extensions
+{
+    public static class AliasExtensions
+    {
+        public static string GetAlias<T>(this T enumType, int index, string fallback = null) where T : struct, IConvertible
+        {
+            var nameAlias = enumType.GetAttribute<AliasAttribute>(enumType.ToString());
+            return nameAlias != null && index < nameAlias.Aliases.Length
+                ? nameAlias.Aliases[index]
+                : fallback;
+        }
+    }
+}
