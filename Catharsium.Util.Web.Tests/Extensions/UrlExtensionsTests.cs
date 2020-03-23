fea98db6ad9540c1fc638076a0da995bc11b43cb
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2020-03-23 09:04:30 +0100

    Add UrlExtensions

---
+++ Catharsium.Util.Web.Tests/Extensions/UrlExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Catharsium.Util.Web.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catharsium.Util.Web.Tests.Extensions
+{
+    [TestClass]
+    public class UrlExtensionsTests
+    {
+        [TestMethod]
+        public void AddQuery_NewVariables_AreAppended()
+        {
+            var url = "My url";
+            var variables = new Dictionary<string, string> {
+                {"Key1", "Value1"},
+                {"Key2", "Value2"}
+            };
+
+            var actual = url.AddQuery(variables);
+            Assert.AreEqual($"{url}?Key1=Value1&Key2=Value2", actual);
+        }
+
+
+        [TestMethod]
+        [Ignore]
+        public void AddQuery_ExistingVariables_AreNotAdded()
+        {
+            var url = "My url?Key1=Value1";
+            var variables = new Dictionary<string, string> {{"Key1", "Value1"}};
+
+            var actual = url.AddQuery(variables);
+            Assert.AreEqual($"{url}?Key1=Value1", actual);
+        }
+
+
+        [TestMethod]
+        [Ignore]
+        public void AddQuery_NewVariables_AreUrlEncoded()
+        {
+            var url = "My url?Key1=Value1";
+            var variables = new Dictionary<string, string> { { "Key1", "Value1" } };
+
+            var actual = url.AddQuery(variables);
+            Assert.AreEqual($"{url}?Key1=Value1", actual);
+        }
+    }
+}
---
+++ Catharsium.Util.Web/Extensions/UrlExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Catharsium.Util.Web.Extensions
+{
+    public static class UrlExtensions
+    {
+        public static string AddQuery(this string url, Dictionary<string, string> variables)
+        {
+            return $"{url}?{string.Join("&", variables.Select(v => $"{v.Key}={v.Value}"))}";
+        }
+    }
+}
