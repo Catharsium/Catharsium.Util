bb9fd38a28cd170deb59ce837ca33f36a0e8fd25
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2020-08-06 15:19:45 +0200

    Release 2.5.5

--- Catharsium.Util/Interfaces/ITypesRetriever.cs
+++ Catharsium.Util/Interfaces/ITypesRetriever.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Catharsium.Util.Interfaces
 {
     public interface ITypesRetriever
     {
+        IEnumerable<Type> GetImplementationsFor<T>(Assembly[] assemblies);
+        IEnumerable<Type> GetImplementationsFor<T>(Assembly assembly);
         IEnumerable<Type> GetImplementationsFor<T>();
     }
 }
