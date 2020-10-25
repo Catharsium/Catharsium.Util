aef4b3e78d0f72070406275ebde456de0bc50515
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2020-10-25 14:11:30 +0100

    Add IJsonFileRepository

---
+++ Catharsium.Util.IO.Tests/Json/JsonFileRepositoryTests.cs
@@ -0,0 +1,83 @@
+﻿using Catharsium.Util.IO.Interfaces;
+using Catharsium.Util.IO.Json;
+using Catharsium.Util.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catharsium.Util.IO.Tests.Json
+{
+    [TestClass]
+    public class JsonFileRepositoryTests : TestFixture<JsonFileRepository<string>>
+    {
+        #region Fixture
+
+        private static string StoragePath => "My storage path";
+        private static string FileName => "My file name";
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.SetDependency(StoragePath, "storagePath");
+        }
+
+        #endregion
+
+        #region LoadAll
+
+        [TestMethod]
+        public async Task LoadAll_ReturnsDataFromAllFiles()
+        {
+            var directory = Substitute.For<IDirectory>();
+            var file1 = Substitute.For<IFile>();
+            var file2 = Substitute.For<IFile>();
+            directory.GetFiles().Returns(new[] { file1, file2 });
+            this.GetDependency<IFileFactory>().CreateDirectory($@"{StoragePath}").Returns(directory);
+            var expected1 = new[] { "First data" };
+            var expected2 = new[] { "Second data" };
+            this.GetDependency<IJsonFileReader>().ReadFrom<IEnumerable<string>>(file1).Returns(expected1);
+            this.GetDependency<IJsonFileReader>().ReadFrom<IEnumerable<string>>(file2).Returns(expected2);
+
+            var actual = await this.Target.LoadAll();
+            Assert.AreEqual(expected1.Length + expected2.Length, actual.Count());
+            foreach (var data in actual)
+            {
+                Assert.IsTrue(expected1.Contains(data) || expected2.Contains(data));
+            }
+        }
+
+        #endregion
+
+        #region Load
+
+        [TestMethod]
+        public async Task Load_ReturnsDataFromFile()
+        {
+            var file = Substitute.For<IFile>();
+            var expected = Array.Empty<string>();
+            this.GetDependency<IFileFactory>().CreateFile($@"{StoragePath}\{FileName}.json").Returns(file);
+            this.GetDependency<IJsonFileReader>().ReadFrom<IEnumerable<string>>(file).Returns(expected);
+
+            var actual = await this.Target.Load(FileName);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+
+        #region Store
+
+        [TestMethod]
+        public async Task Store_WritesDataToFile()
+        {
+            var data = Array.Empty<string>();
+            await this.Target.Store(data, FileName);
+            this.GetDependency<IJsonFileWriter>().Received().Write(data, $@"{StoragePath}\{FileName}.json");
+        }
+
+        #endregion
+    }
+}
---
+++ Catharsium.Util.IO/Json/JsonFileRepository.cs
@@ -0,0 +1,65 @@
+﻿using Catharsium.Util.IO.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catharsium.Util.IO.Json
+{
+    public class JsonFileRepository<T> : IJsonFileRepository<T>
+    {
+        private readonly IFileFactory fileFactory;
+        private readonly IJsonFileReader jsonFileReader;
+        private readonly IJsonFileWriter jsonFileWriter;
+        private readonly string storagePath;
+
+
+        public JsonFileRepository(
+            IFileFactory fileFactory,
+            IJsonFileReader jsonFileReader,
+            IJsonFileWriter jsonFileWriter,
+            string storagePath)
+        {
+            this.fileFactory = fileFactory;
+            this.jsonFileReader = jsonFileReader;
+            this.jsonFileWriter = jsonFileWriter;
+            this.storagePath = storagePath;
+        }
+
+
+        public async Task<IEnumerable<T>> LoadAll()
+        {
+            return await Task.Run<IEnumerable<T>>(() =>
+            {
+                var directory = this.fileFactory.CreateDirectory($@"{this.storagePath}");
+                var result = new List<T>();
+                foreach (var file in directory.GetFiles())
+                {
+                    result.AddRange(this.Load(file));
+                }
+
+                return result;
+            });
+        }
+
+
+        public async Task<IEnumerable<T>> Load(string fileName)
+        {
+            return await Task.Run(() =>
+            {
+                var file = this.fileFactory.CreateFile($@"{this.storagePath}\{fileName}.json");
+                return this.Load(file);
+            });
+        }
+
+
+        private IEnumerable<T> Load(IFile file)
+        {
+            return this.jsonFileReader.ReadFrom<IEnumerable<T>>(file);
+        }
+
+
+        public async Task Store(IEnumerable<T> data, string fileName)
+        {
+            await Task.Run(() => this.jsonFileWriter.Write(data, $@"{this.storagePath}\{fileName}.json"));
+        }
+    }
+}
