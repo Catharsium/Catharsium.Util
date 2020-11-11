a9d8fb3563f5e2ffa409a316cdc100aea222f833
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2020-11-11 09:36:18 +0100

    Add GetWeekOfYear extension method

---
+++ Catharsium.Util.Tests/Time/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Catharsium.Util.Time.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Catharsium.Util.Tests.Time.Extensions
+{
+    [TestClass]
+    public class DateTimeExtensionsTests
+    {
+        [TestMethod]
+        public void GetWeekOfYear_FirstDayOfYear_Returns1()
+        {
+            var date = new DateTime(2020, 1, 1);
+            var actual = date.GetWeekOfYear();
+            Assert.AreEqual(1, actual);
+        }
+
+
+        [TestMethod]
+        public void GetWeekOfYear_SpecificDay_ReturnsWeekNumber()
+        {
+            var date = new DateTime(2020, 11, 10);
+            var actual = date.GetWeekOfYear();
+            Assert.AreEqual(46, actual);
+        }
+
+
+        [TestMethod]
+        public void GetWeekOfYear_LastDayOfYear_ReturnsLastWeekNumber()
+        {
+            var date = new DateTime(2020, 12, 31);
+            var actual = date.GetWeekOfYear();
+            Assert.AreEqual(53, actual);
+        }
+    }
+}
---
+++ Catharsium.Util/Time/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Catharsium.Util.Time.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int GetWeekOfYear(this DateTime date)
+        {
+            var cultureInfo = new CultureInfo("nl-NL");
+            var calendar = cultureInfo.Calendar;
+            var calendarWeekRule = cultureInfo.DateTimeFormat.CalendarWeekRule;
+            var firstDayOfWeek = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+            return calendar.GetWeekOfYear(date, calendarWeekRule, firstDayOfWeek);
+        }
+    }
+}
