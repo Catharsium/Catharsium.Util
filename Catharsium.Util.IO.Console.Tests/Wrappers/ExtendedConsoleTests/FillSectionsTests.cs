e01e5084e6a1757e7a44b443704fd90a0f96da0b
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2021-05-16 10:42:20 +0200

    Fix tests

--- Catharsium.Util.IO.Console.Tests/Wrappers/ExtendedConsoleTests/FillSectionsTests.cs
+++ Catharsium.Util.IO.Console.Tests/Wrappers/ExtendedConsoleTests/FillSectionsTests.cs
@@ -10,44 +10,44 @@ namespace Catharsium.Util.IO.Console.Tests.Wrappers.ExtendedConsoleTests
     public class FillSectionsTests : TestFixture<ExtendedConsole>
     {
         [TestMethod]
-        public void FillSection_TextLengthLessThanSecion_AddsFiller()
+        public void FillBlock_TextLengthLessThanSecion_AddsFiller()
         {
             var text = "My text";
             var fillerLength = 3;
             var sectionLength = text.Length + fillerLength;
             var filler = '!';
 
-            this.Target.FillSection(text.Length, sectionLength, filler);
+            this.Target.FillBlock(text.Length, sectionLength, filler);
             this.GetDependency<IConsoleWrapper>().Received(fillerLength).Write(filler.ToString());
         }
 
 
         [TestMethod]
-        public void FillSection_NoFiller_UsesDefault()
+        public void FillBlock_NoFiller_UsesDefault()
         {
             var text = "My text";
             var fillerLength = 3;
             var sectionLength = text.Length + fillerLength;
 
-            this.Target.FillSection(text.Length, sectionLength);
+            this.Target.FillBlock(text.Length, sectionLength);
             this.GetDependency<IConsoleWrapper>().Received(fillerLength).Write(' '.ToString());
         }
 
 
         [TestMethod]
-        public void FillSection_NoSectionLength_UsesDefault()
+        public void FillBlock_NoSectionLength_UsesDefault()
         {
             var text = "12345";
-            this.Target.FillSection(text.Length);
+            this.Target.FillBlock(text.Length);
             this.GetDependency<IConsoleWrapper>().Received(8 - text.Length).Write(' '.ToString());
         }
 
 
         [TestMethod]
-        public void FillSection_TextLengthEqualToSecion_AddsFiller()
+        public void FillBlock_TextLengthEqualToSecion_AddsFiller()
         {
             var text = "My text";
-            this.Target.FillSection(text.Length, text.Length);
+            this.Target.FillBlock(text.Length, text.Length);
             this.GetDependency<IConsoleWrapper>().DidNotReceive().Write(Arg.Any<string>());
         }
     }
