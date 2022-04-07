1607d6247b66897d47e11cdd4b34ccbbf2ca477f
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2022-04-07 10:59:54 +0200

    Add IServiceCollection.RegisterTypes extension methods

--- Configuration/Catharsium.Util.Configuration.Tests/Extensions/ServiceCollectionExtensionsTests.cs
+++ Configuration/Catharsium.Util.Configuration.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -1,13 +1,19 @@
 ﻿using Catharsium.Util.Configuration.Extensions;
+using Catharsium.Util.Configuration.Tests._Mocks;
 using Catharsium.Util.Configuration.Tests.Extensions._Fixture;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using System;
+using System.Collections.Generic;
+
 namespace Catharsium.Util.Configuration.Tests.Extensions;
 
 [TestClass]
 public class ServiceCollectionExtensionsTests
 {
+    #region RegisterAll
+
     [TestMethod]
     public void RegisterAll_FindsTypesThatImplementBase_RegistersTypes()
     {
@@ -17,4 +23,52 @@ public class ServiceCollectionExtensionsTests
         services.RegisterAll<IMockInterface>(lifetime, typeof(MockObject).Assembly);
         services.Received().Add(Arg.Is<ServiceDescriptor>(s => s.ServiceType == typeof(IMockInterface) && s.ImplementationType == typeof(MockObject) && s.Lifetime == lifetime));
     }
+
+    #endregion
+
+    #region RegisterTypes (Dictionary)
+
+    [TestMethod]
+    public void RegisterTypes_DictionaryOfTypes_RegistersTypes()
+    {
+        var services = Substitute.For<IServiceCollection>();
+        var lifetime = ServiceLifetime.Scoped;
+        var types = new Dictionary<string, string> {
+            { typeof(ImplementationType).AssemblyQualifiedName, typeof(IType).AssemblyQualifiedName }
+        };
+
+        services.RegisterTypes(types, lifetime);
+        foreach (var type in types) {
+            services.Received().Add(Arg.Is<ServiceDescriptor>(sd =>
+                sd.ServiceType == Type.GetType(type.Value) &&
+                sd.ImplementationType == Type.GetType(type.Key) &&
+                sd.Lifetime == lifetime
+            ));
+        }
+    }
+
+    #endregion
+
+    #region RegisterTypes (List)
+
+    [TestMethod]
+    public void RegisterTypes_ListOfTypes_RegistersTypes()
+    {
+        var services = Substitute.For<IServiceCollection>();
+        var lifetime = ServiceLifetime.Scoped;
+        var types = new List<string> {
+            typeof(ImplementationType).AssemblyQualifiedName
+        };
+
+        services.RegisterTypes<IType>(types, lifetime);
+        foreach (var type in types) {
+            services.Received().Add(Arg.Is<ServiceDescriptor>(sd =>
+                sd.ServiceType == typeof(IType) &&
+                sd.ImplementationType == Type.GetType(type) &&
+                sd.Lifetime == lifetime
+            ));
+        }
+    }
+
+    #endregion
 }
--- Configuration/Catharsium.Util.Configuration/Extensions/ServiceCollectionExtensions.cs
+++ Configuration/Catharsium.Util.Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -1,21 +1,42 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-
 namespace Catharsium.Util.Configuration.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void RegisterAll<T>(this IServiceCollection services, ServiceLifetime lifetime, params Assembly[] assemblies)
+    public static IServiceCollection RegisterAll<T>(this IServiceCollection services, ServiceLifetime lifetime, params Assembly[] assemblies)
     {
         var typesFromAssemblies = assemblies.SelectMany(a => a.DefinedTypes.Where(t => t.GetInterfaces().Contains(typeof(T))));
         foreach (var type in typesFromAssemblies) {
             services.Add(new ServiceDescriptor(typeof(T), type, lifetime));
         }
+
+        return services;
     }
 
 
-    public static void RegisterAll<T>(this IServiceCollection services, params Assembly[] assemblies)
+    public static IServiceCollection RegisterAll<T>(this IServiceCollection services, params Assembly[] assemblies)
     {
-        services.RegisterAll<T>(ServiceLifetime.Transient, assemblies);
+        return services.RegisterAll<T>(ServiceLifetime.Transient, assemblies);
+    }
+
+
+    public static IServiceCollection RegisterTypes(this IServiceCollection services, Dictionary<string, string> types, ServiceLifetime lifetime = ServiceLifetime.Transient)
+    {
+        foreach (var type in types) {
+            services.Add(new ServiceDescriptor(Type.GetType(type.Value), Type.GetType(type.Key), lifetime));
+        }
+
+        return services;
+    }
+
+
+    public static IServiceCollection RegisterTypes<T>(this IServiceCollection services, IEnumerable<string> types, ServiceLifetime lifetime = ServiceLifetime.Transient)
+    {
+        foreach (var type in types) {
+            services.Add(new ServiceDescriptor(typeof(T), Type.GetType(type), lifetime));
+        }
+
+        return services;
     }
 }
