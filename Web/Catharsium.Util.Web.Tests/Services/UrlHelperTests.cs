5896b6cb29ffa437b0ba6f68286fcae882480d26
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2022-07-23 09:29:11 +0200

    Force build

--- Web/Catharsium.Util.Web.Tests/Services/UrlHelperTests.cs
+++ Web/Catharsium.Util.Web.Tests/Services/UrlHelperTests.cs
@@ -5,7 +5,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 namespace Catharsium.Util.Web.Tests.Services;
 
-[Ignore]
 [TestClass]
 public class UrlHelperTests : TestFixture<UrlHelper>
 {
