ec45865c4096158d9d1b16d2d25673783898c0b6
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2022-08-17 08:19:48 +0200

    Make menu title configurable

--- IO/Catharsium.Util.IO.Console/Menu/Base/BaseMenuActionHandler.cs
+++ IO/Catharsium.Util.IO.Console/Menu/Base/BaseMenuActionHandler.cs
@@ -9,16 +9,19 @@ namespace Catharsium.Util.IO.Console.Menu.Base;
 public abstract class BaseMenuActionHandler<T> : BaseActionHandler, IMenuActionHandler where T : IActionHandler
 {
     protected readonly IEnumerable<T> ActionHandlers;
+    private readonly string title;
 
 
-    public BaseMenuActionHandler(IEnumerable<T> actionHandlers, IConsole console, string menuName) : base(console, menuName) {
+    public BaseMenuActionHandler(IEnumerable<T> actionHandlers, IConsole console, string menuName, string title = "Please select an action:")
+        : base(console, menuName) {
         this.ActionHandlers = actionHandlers.ToList();
+        this.title = title;
     }
 
 
     public override async Task Run() {
         while (true) {
-            this.Console.WriteLine("Please select an action:");
+            this.Console.WriteLine(this.title);
             var index = 1;
             foreach (var actionHandler in this.ActionHandlers) {
                 this.Console.WriteLine($"[{index++}] {actionHandler.MenuName}");
