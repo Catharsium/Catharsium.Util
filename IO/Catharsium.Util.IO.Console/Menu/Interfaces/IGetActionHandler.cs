41697055845b01283100c9e80c82c66b8286446a
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2022-08-26 11:29:12 +0200

    Add IGetActionHandler.Select overload to receive a list

--- IO/Catharsium.Util.IO.Console/Menu/Interfaces/IGetActionHandler.cs
+++ IO/Catharsium.Util.IO.Console/Menu/Interfaces/IGetActionHandler.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Catharsium.Util.IO.Console.Menu.Interfaces;
 
 public interface IGetActionHandler<T>
 {
     Task<T> Select();
+    Task<T> Select(IEnumerable<T> items);
 }
