b8c4138e74d11d9e64c80dacdf7ecedb2243a21d
Thierry Brachthuizer <t.w.brachthuizer@gmail.com>
2024-11-01 10:31:51 +0100

    Update packages

---
+++ IO/Catharsium.Util.IO.Files/Csv/CsvOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catharsium.Util.IO.Files.Csv;
+internal class CsvOptions
+{
+}
